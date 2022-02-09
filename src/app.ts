3829d16669ad4746558f2d5450a885ceb7a5ab0f
iamLolade <adeniyiotemade@gmail.com>
2022-02-09 02:52:37 +0100

    Created user-interface and initialized tsc

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import { Invoice } from "./classes/Invoice.js"
+import { Payment } from "./classes/Payment.js";
+import { Hasformatter } from "./interfaces/HasFormatter.js";
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+
+form.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+
+    let doc: Hasformatter;
+    if(type.value === "invoice") {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber)
+    } else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber)
+    }
+    console.log(doc)
+
+})
